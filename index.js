ffcae6cc8e0dcfd14cccc5b0aafc3c347b6efce0
unknown <mdyasirofficial2001@gmail.com>
2022-10-25 11:37:13 +0600

    cors added

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+
 const port = process.env.PORT || 5000;
-
-app.use(cors());
-
-//Course Data
-const courses = require("./data/courses.json");
-
-// API Loading
-app.get("/courses", (req, res) => {
-  res.send(courses);
-});
 
 // Initial message
 app.get("/", (req, res) => {
