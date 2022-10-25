057f254bc5cefd9660e962f593cd5a06949299f7
unknown <mdyasirofficial2001@gmail.com>
2022-10-25 11:37:37 +0600

    API loaded

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+//Course Data
+const courses = require("./data/courses.json");
+
+// API Loading
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
 // Initial message
 app.get("/", (req, res) => {
